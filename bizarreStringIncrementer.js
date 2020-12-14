fc0e5399e8be9d60e3b513bc8bf62bb788a83c14
nishantbisen09 <nishantbisen09@gmail.com>
2020-12-14 17:49:32 +0530

    Refactor Code into modular functions

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,34 +1,49 @@
 const bizarreStringIncrementer = (bizarreString) => {
   if (typeof bizarreString !== "string") throw "Invalid input!";
   const splittedInput = bizarreString.split("");
+  const trailingNumber = getTrailingNumber(splittedInput);
+  const preceedingString = bizarreString.replace(trailingNumber, "");
+  return preceedingString + getIncrementedNumber(trailingNumber);
+};
+
+const getTrailingNumber = (splittedInput) => {
   let trailingNumbersArray = [];
   splittedInput.forEach((character) => {
     if (isAlphabet(character) && trailingNumbersArray.length > 0)
       trailingNumbersArray = [];
     if (isNumber(character)) trailingNumbersArray.push(character);
   });
-  let trailingNumber = trailingNumbersArray.join("");
-  const preceedingString = bizarreString.replace(trailingNumber, "");
-  const result = preceedingString + getIncrementedNumber(trailingNumber);
-  return result;
+  return trailingNumbersArray.join("");
 };
 
 const getIncrementedNumber = (numberString) => {
   if (hasPreceedingZeros(numberString)) {
-    let noOfZeros = 0;
-    numberString.split("").forEach((item) => item === "0" && noOfZeros++);
+    let noOfZeros = getNoOfZeros(numberString);
     const numberToBeIncremented = parseInt(numberString, 10);
     const incrementedNumber = numberToBeIncremented + 1;
-    if (
-      incrementedNumber.toString().length >
-        numberToBeIncremented.toString().length &&
+    shouldRemoveExtraZero(
+      incrementedNumber,
+      numberToBeIncremented,
       noOfZeros
-    )
-      noOfZeros--;
+    ) && noOfZeros--;
     return padZeros(incrementedNumber, noOfZeros);
   } else {
     return numberString ? parseInt(numberString, 10) + 1 : 1;
   }
+};
+
+const shouldRemoveExtraZero = (
+  incrementedNumber,
+  numberToBeIncremented,
+  noOfZeros
+) =>
+  incrementedNumber.toString().length >
+    numberToBeIncremented.toString().length && noOfZeros;
+
+const getNoOfZeros = (numberString) => {
+  let noOfZeros = 0;
+  numberString.split("").forEach((item) => item === "0" && noOfZeros++);
+  return noOfZeros;
 };
 
 const padZeros = (numberString, noOfZeros) => {
@@ -43,11 +58,5 @@ const padZeros = (numberString, noOfZeros) => {
 const isNumber = (character) => /[0-9]+/g.test(character);
 const isAlphabet = (character) => /[A-Za-z]+/g.test(character);
 const hasPreceedingZeros = (text) => /^(0)+([1-9]*)$/g.test(text);
-
-try {
-  bizarreStringIncrementer("foo123");
-} catch (error) {
-  console.log(error);
-}
 
 module.exports = { bizarreStringIncrementer };
